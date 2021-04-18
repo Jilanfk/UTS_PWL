fd742f55ead71eb295d8e74f63fd980647756810
Jilanfk <jilanfk@gmail.com>
2021-04-19 06:57:26 +0700

    Update home.php

--- UTS_PWL/home.php
+++ UTS_PWL/home.php
@@ -2,7 +2,7 @@
     <h1 class="display-4">iya.</h1>
     <p class="lead">Nama    :   Jilan Finian Kilani</p>
     <p class="lead">NIM     :   0110219103</p>
-    <p class="lead">Kelas   :   5 TI-01</p>
+    <p class="lead">Kelas   :   4 TI-01</p>
     <hr class="my-4">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quibusdam quas nisi harum odio laudantium fuga! Et, fuga nobis corporis, adipisci ullam harum veniam magnam rerum ipsam deleniti quae quos.</p>
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
