01be3d22cd6b3e2dc3352f443baf3ce8679a29d8
Jilanfk <jilanfk@gmail.com>
2021-04-19 06:54:43 +0700

    Add files via upload

---
+++ UTS_PWL/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="alert bg-primary" role="alert">
+            Created By Jilan Finian Kilani | &#169; 2021 All Rights Reserved
+        </div>
+    </div>
+</div>
